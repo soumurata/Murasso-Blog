11f8b67c1d4b0359422cbdaf5dd0b20dbff044d1
soumurata <murasso.soccer.0802@gmail.com>
2022-05-13 23:47:56 +0900

    apply markdown

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
+from mdeditor.fields import MDTextField
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = MDTextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
